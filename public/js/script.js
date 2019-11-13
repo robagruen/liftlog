571713a3fd2749ecf3fa4b1a6c4f1be85af7c9ae
Rob <rob@Robs-MacBook-Pro-2.local>
2019-11-13 18:11:54 -0500

    Okay one more thing

--- public/js/script.js
+++ public/js/script.js
@@ -9,5 +9,12 @@ window.onload = function () {
         }
     }
 
+    let subheadings = document.getElementsByClassName("liftlog-list-subheading");
+    for (let i = 0; i < subheadings.length; i++) {
+        if (subheadings[i].innerText == "|") {
+            subheadings[i].innerText = "| Uncategorized |";
+        }
+    }
+
     document.getElementById("category_count").value = document.getElementsByClassName("category_checkboxes").length;
 }
